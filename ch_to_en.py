366bae711cd436f3b79b448ad3f98b9c959b2f74
Skywalker <imsho0218@gmail.com>
2023-11-21 20:36:43 +0800

    add statistics

--- ch_to_en.py
+++ ch_to_en.py
@@ -5,6 +5,7 @@ import csv
 import os
 import threading
 
+# add pronunciation
 def speak(text):
     tts = gTTS(text=text, lang='en')
     filename = 'word.mp3'
@@ -16,7 +17,7 @@ def play_audio(filename):
         subprocess.run(['ffplay', '-nodisp', '-autoexit', filename], stdout=devnull, stderr=devnull)
 
 study_set = {}
-dataset = input("Which set you want to learn?  ")
+dataset = input("Which set you want to learn? (enter file's name) ")
 
 # read csv file
 with open(f'{dataset}.csv', mode ='r', encoding='utf-8')as file:
@@ -26,6 +27,10 @@ with open(f'{dataset}.csv', mode ='r', encoding='utf-8')as file:
 
 keys = list(study_set.keys())
 random.shuffle(keys)
+
+# statistics
+correct = 0
+wrong = 0
 
 # add options
 for key in keys:
@@ -45,11 +50,13 @@ for key in keys:
     
 
     if question[int(response)] == meaning:
+        correct += 1
         play_audio('word.mp3')
         print("Correct!\n")
         print("\033[1;37;42m ======================================================= \033[0m\n")
     else:
         print(f"Wrong! The correct answer is '{meaning}'.\n")
+        wrong += 1
         while True:
             threading.Thread(target=play_audio, args=('word.mp3',)).start()
             retry = input("Please spell the word again ! ans: ")
@@ -59,6 +66,7 @@ for key in keys:
         keys.append(word)
         print("\033[1;37;41m ======================================================= \033[0m\n")
 
-    
+# show statistics
+print(f"Correct: {correct}\nWrong: {wrong}\nAccuracy: {round(correct/(correct+wrong),2)}\n")
 
     
