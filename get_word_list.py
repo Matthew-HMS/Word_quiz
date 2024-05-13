0bfae0b50b976408382326ff84a0e531eb3ee166
Skywalker <imsho0218@gmail.com>
2024-05-13 19:55:48 +0800

    FIX: show more button, bot detection

--- get_word_list.py
+++ get_word_list.py
@@ -1,37 +1,41 @@
-import time
-from selenium import webdriver
+from seleniumbase import Driver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import csv
+import time
 
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(options=options)
-driver.get("https://quizlet.com/tw/857640181/%E6%89%98%E7%A6%8F%E5%88%86%E9%A1%9E%E5%AD%97%E5%BD%9903-anthropologyarchaeology-flash-cards/?new")
 
-word_list = driver.find_elements(By.XPATH, "//*[@class='SetPageTerm-wordText']")
-definition_list = driver.find_elements(By.XPATH, "//*[@class='SetPageTerm-definitionText']")
+driver = Driver(browser='chrome', headless=False, uc=True)
+wait = WebDriverWait(driver, 10)  # Wait up to 10 seconds
 
-words = []
-definitions = []
-for word in word_list:
-    words.append(word.text)
+driver.get("https://quizlet.com/tw/498049178/%E5%AD%97%E5%BD%99%E5%A1%AB%E7%A9%BA%E4%BA%8C-flash-cards/")
 
-for definition in definition_list:
-    definitions.append(definition.text)
+while True:
+    try:
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@class='AssemblyButtonBase AssemblyTextButton AssemblyTextButton--secondary AssemblyButtonBase--large AssemblyButtonBase--padding']")))
+        button.click()
+        time.sleep(3)
+    except:
+        break
+
+word_list = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='TermText notranslate lang-en']")))
+definition_list = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='TermText notranslate lang-zh-TW']")))
+
+words = [word.text for word in word_list]
+definitions = [definition.text for definition in definition_list]
 
 word_dict = dict(zip(words, definitions))
 
 # Specify the file name
-filename = './sets/toefl_anthropology.csv'
+filename = './sets/test2.csv'
 with open(filename, 'w', newline='', encoding='utf-8') as csv_file:  
     writer = csv.writer(csv_file)
     # Write the data
     for key, value in word_dict.items():
-       cleaned_key = key.replace('\n', '; ')
-       cleaned_key = cleaned_key.replace(',', '，')
-       cleaned_value = value.replace('\n', '; ')
-       cleaned_value = cleaned_value.replace(',', '，')
+       cleaned_key = key.replace('\n', '; ').replace(',', '，')
+       cleaned_value = value.replace('\n', '; ').replace(',', '，')
        writer.writerow([cleaned_key, cleaned_value])
-
 
 time.sleep(3)
 driver.quit()
